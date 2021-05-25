5471ce28e13440070856200ed56a66a65e3a75d2
limawanyan <2388962411@qq.com>
2021-05-25 14:02:53 +0800

    SlidingWindowMaximum

---
+++ Heap/239.SlidingWindowMaximum/solution.go
@@ -0,0 +1,122 @@
+package _39_SlidingWindowMaximum
+
+import (
+	"container/heap"
+	"sort"
+)
+
+// 暴力破解法(超时时间限制)
+func maxSlidingWindow(nums []int, k int) []int {
+	result := make([]int,0,k)
+	length := len(nums)
+	if length == 0 {
+		return []int{}
+	}
+	for i := 0;i <= length-k;i++{
+		max := nums[i]
+		for j := 1;j < k;j++{
+			if max < nums[i + j]{
+				max = nums[i+j]
+			}
+		}
+		result = append(result,max)
+	}
+	return result
+}
+
+// 优先队列 大顶堆
+var a []int
+type hp struct{sort.IntSlice}
+func (h *hp) Less(i,j int) bool{
+	return a[h.IntSlice[i]] > a[h.IntSlice[j]]
+}
+func (h *hp) Push(v interface{})  {
+	h.IntSlice = append(h.IntSlice,v.(int))
+}
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
+
+func maxSlidingWindow2(nums []int, k int) []int {
+	a = nums
+	q := &hp{make([]int,k)}
+	for i := 0;i < k;i++{
+		q.IntSlice[i] = i
+	}
+	heap.Init(q)
+
+	n := len(nums)
+	ans := make([]int,1,n-k+1)
+	ans[0] = nums[q.IntSlice[0]]
+	for i := k;i < n;i++{
+		heap.Push(q,i)
+		for q.IntSlice[0] <= i-k{
+			heap.Pop(q)
+		}
+		ans = append(ans,nums[q.IntSlice[0]])
+	}
+	return ans
+}
+
+// 解法三 双端队列
+func maxSlidingWindow3(nums []int,k int) []int  {
+	if len(nums) == 0 || len(nums) < k{
+		return make([]int,0)
+	}
+	// 存储索引
+	window := make([]int,0,k)
+	result := make([]int,0,len(nums)-k+1)
+	for i,v := range nums {
+		if i >= k && window[0] <= i-k{
+			window = window[1:len(window)]
+		}
+		// 维护窗口
+		for len(window) > 0 && nums[window[len(window)-1]] < v{
+			window = window[0:len(window)-1]
+		}
+		// 存储索引到数组
+		window = append(window,i)
+		if i >= k-1{
+			result = append(result,nums[window[0]])
+		}
+	}
+	return result
+}
+
+// 解法四 分块 + 预处理
+func maxSlidingWindow4(nums []int, k int) []int {
+	n := len(nums)
+	prefixMax := make([]int,n)
+	suffixMax := make([]int,n)
+	for i,v := range  nums{
+		if i % k == 0{
+			prefixMax[i] = v
+		}else {
+			prefixMax[i] = max(prefixMax[i-1],v)
+		}
+	}
+	for i := n - 1;i >= 0;i--{
+		if i == n-1 || (i+1)%k == 0{
+			suffixMax[i] = nums[i]
+		}else{
+			suffixMax[i] = max(suffixMax[i+1],nums[i])
+		}
+	}
+
+	ans := make([]int,n-k+1)
+	for i := range ans{
+		ans[i] = max(suffixMax[i],prefixMax[i+k-1])
+	}
+	return ans
+
+}
+
+func max(a,b int) int {
+	if a > b{
+		return a
+	}
+	return b
+}
---
+++ Heap/239.SlidingWindowMaximum/solution_test.go
@@ -0,0 +1,11 @@
+package _39_SlidingWindowMaximum
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	fmt.Println(maxSlidingWindow3([]int{1,2,3,9,5,6,4},3))
+	fmt.Println(maxSlidingWindow4([]int{1,2,3,9,5,6,4},3))
+}
